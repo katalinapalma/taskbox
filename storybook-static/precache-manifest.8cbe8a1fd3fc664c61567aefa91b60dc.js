be0a83484ab264f5a2dd0f45b1f940b9a9e83397
katalinapalma <kata_92@hotmail.com>
2020-10-07 10:24:37 +0200

    taskbox UI

---
+++ storybook-static/precache-manifest.8cbe8a1fd3fc664c61567aefa91b60dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2436e58450afbd1bee4d1519b5594d0f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d085060eeb583ee28078.bundle.js"
+  },
+  {
+    "url": "runtime~main.d085060eeb583ee28078.bundle.js"
+  },
+  {
+    "revision": "bafdb6cb071de6092359",
+    "url": "static/css/main.f2b987d9.chunk.css"
+  },
+  {
+    "url": "vendors~main.d085060eeb583ee28078.bundle.js"
+  },
+  {
+    "url": "vendors~main.d085060eeb583ee28078.bundle.js.LICENSE.txt"
+  }
+]);
